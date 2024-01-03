795f6b925621045a6955586716436439752386a7
이다빈 <dlekqls6204@naver.com>
2024-01-04 02:16:10 +0900

    test/CICD

---
+++ src/main/java/com/example/aboutbook/TestController.java
@@ -0,0 +1,26 @@
+package com.example.aboutbook;
+
+import com.example.aboutbook.global.exception.ApiResponse;
+import com.example.aboutbook.user.dto.UserReq;
+import com.example.aboutbook.user.dto.UserRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name="Test❤", description = "Test Api")
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    @Operation(summary = "테스트", description = "테스트!")
+    public ApiResponse<String> signup() throws Exception {
+        try{
+            return new ApiResponse<>("test success.");
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+}
